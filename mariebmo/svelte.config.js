1bee7b16448da298318dec740b8b04968b7eba8e
mariestigen <marie.stigen@hotmail.com>
2023-10-17 21:47:24 +0200

    again

--- mariebmo/svelte.config.js
+++ mariebmo/svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({pages: 'build', assets: 'build', fallback: null})
 	}
 };
 
