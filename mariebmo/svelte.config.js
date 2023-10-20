0d01ab4fa11dfb969e82d2e0ce06673a23f049ff
Marie Blichfeldt Mo <marie.stigen@hotmail.com>
2023-10-20 12:06:42 +0200

    Develop (#2)
    * Update merge.yml (#1)
    
    * Moving to Vercel instead of pages
    
    * Fixed tests

--- mariebmo/svelte.config.js
+++ mariebmo/svelte.config.js
@@ -1,12 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: adapter({pages: 'build', assets: 'build', fallback: null})
+		adapter: adapter({
+			// see the 'Deployment configuration' section below
+		})
 	}
 };
-
-config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
-
-export default config;
