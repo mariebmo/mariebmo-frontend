dc47ed8b07806e895a3c9ca5de04f1b8def958c5
mariestigen <marie.stigen@hotmail.com>
2023-10-24 12:32:39 +0200

    prettier

--- mariebmo/src/routes/knitting/+page.svelte
+++ mariebmo/src/routes/knitting/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-
 	var current = 12;
 	var amount = 8;
 	var totalAmountIncluded = false;
-    var increaseSelected = true;
+	var increaseSelected = true;
 	$: byOrTo = totalAmountIncluded ? 'to' : 'by';
-    $: increaseOrDecrease = increaseSelected ? 'increase' : 'decrease';
+	$: increaseOrDecrease = increaseSelected ? 'increase' : 'decrease';
 	var visualizationOutput = '';
 	var knittingLingoOutput = '';
 
@@ -13,24 +12,35 @@
 		totalAmountIncluded = !totalAmountIncluded;
 	}
 
-    function toggleIncreaseDecrease(){
-        increaseSelected = !increaseSelected;
-    }
+	function toggleIncreaseDecrease() {
+		increaseSelected = !increaseSelected;
+	}
 
 	function increase() {
 		var increaseAmount = totalAmountIncluded ? amount - current : amount;
 
-        if(totalAmountIncluded && current > amount){
-            visualizationOutput = "You can't increase from " + current + " to " + amount + " stitches, silly! Did you mean to decrease or increase by " + amount + " stitches?";
-            knittingLingoOutput = '';
-            return;
-        }
+		if (totalAmountIncluded && current > amount) {
+			visualizationOutput =
+				"You can't increase from " +
+				current +
+				' to ' +
+				amount +
+				' stitches, silly! Did you mean to decrease or increase by ' +
+				amount +
+				' stitches?';
+			knittingLingoOutput = '';
+			return;
+		}
 
-        if(totalAmountIncluded && (amount - current) > current || !totalAmountIncluded && amount > current ){
-            visualizationOutput = "This calculator doesn't support increasing by more than the current amount of stitches."
-            knittingLingoOutput = '';
-            return;
-        }
+		if (
+			(totalAmountIncluded && amount - current > current) ||
+			(!totalAmountIncluded && amount > current)
+		) {
+			visualizationOutput =
+				"This calculator doesn't support increasing by more than the current amount of stitches.";
+			knittingLingoOutput = '';
+			return;
+		}
 
 		visualizationOutput = '';
 		knittingLingoOutput = '';
@@ -60,7 +70,6 @@
 				increaseCount++;
 				nextIncrease += increaseRatio;
 				increaseRemainder--;
-
 			} else {
 				if (!isCurrentKnit) {
 					var increaseCountOutput = increaseCount > 1 ? increaseCount : '';
@@ -77,15 +86,15 @@
 			visualizationOutput += ' ';
 		}
 
-        if(increaseCount > 0){
-            var knitCountOutput = increaseCount > 1 ? increaseCount : '';
-            knittingLingoOutput += 'inc' + knitCountOutput + ' ';
-        }
+		if (increaseCount > 0) {
+			var knitCountOutput = increaseCount > 1 ? increaseCount : '';
+			knittingLingoOutput += 'inc' + knitCountOutput + ' ';
+		}
 
-        if(knitCount > 0){
-            var knitCountOutput = knitCount > 1 ? knitCount : '';
-            knittingLingoOutput += 'k' + knitCountOutput + ' ';
-        }
+		if (knitCount > 0) {
+			var knitCountOutput = knitCount > 1 ? knitCount : '';
+			knittingLingoOutput += 'k' + knitCountOutput + ' ';
+		}
 	}
 
 	function decrease() {
@@ -95,82 +104,93 @@
 		var decreaseAmount = totalAmountIncluded ? current - amount : amount;
 	}
 
-    function submit(){
-        if(increaseSelected){
-            increase()
-        } else {
-            decrease()
-        }
-    }
+	function submit() {
+		if (increaseSelected) {
+			increase();
+		} else {
+			decrease();
+		}
+	}
 </script>
 
-<div id='knitting-page'>
-    <div id='knitting-calculator-content'>
+<div id="knitting-page">
+	<div id="knitting-calculator-content">
+		<div id="knitting-calculator-search">
+			<label class="calculator-element" for="current">from</label>
+			<input
+				id="current"
+				class="input-number calculator-element"
+				type="number"
+				bind:value={current}
+			/>
 
-    <div id='knitting-calculator-search'>
-        <label class='calculator-element' for="current">from</label>
-        <input id="current" class="input-number calculator-element" type="number" bind:value={current} />
-    
-        <button on:click={toggleIncreaseDecrease} id='increase-decrease-btn' class='calculator-element'>{increaseOrDecrease}</button>
-        <button on:click={toggleByOrTo} id='by-to-btn' class="calculator-element">{byOrTo}</button>
-    
-        <input id="amount" class="input-number calculator-element" type="number" bind:value={amount} />
-    
-        <button class="calculator-element" on:click={submit}>Search</button>
-    </div>
+			<button
+				on:click={toggleIncreaseDecrease}
+				id="increase-decrease-btn"
+				class="calculator-element">{increaseOrDecrease}</button
+			>
+			<button on:click={toggleByOrTo} id="by-to-btn" class="calculator-element">{byOrTo}</button>
 
-	<div>
-		<div class="increase-output">
-            <p>{visualizationOutput}</p>
-			<p>{knittingLingoOutput}</p>
-        </div>
+			<input
+				id="amount"
+				class="input-number calculator-element"
+				type="number"
+				bind:value={amount}
+			/>
+
+			<button class="calculator-element" on:click={submit}>Search</button>
+		</div>
+
+		<div>
+			<div class="increase-output">
+				<p>{visualizationOutput}</p>
+				<p>{knittingLingoOutput}</p>
+			</div>
+		</div>
 	</div>
-</div>
 </div>
 
 <style>
 	.increase-output {
-        margin-top: 1rem;
+		margin-top: 1rem;
 		font-family: monospace;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 
 	.input-number {
 		width: 3rem;
 	}
 
-    #knitting-page {
-        width: 100%;
-    }
+	#knitting-page {
+		width: 100%;
+	}
 
-    #knitting-calculator-content {
-        align-items: center;
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-    }
+	#knitting-calculator-content {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+	}
 
-    #knitting-calculator-search {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-    }
+	#knitting-calculator-search {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+	}
 
-    #increase-decrease-btn {
-        min-width: 75px;
-    }
+	#increase-decrease-btn {
+		min-width: 75px;
+	}
 
-    #by-to-btn {
-        min-width: 30px;
-    }
+	#by-to-btn {
+		min-width: 30px;
+	}
 
-    .calculator-element {
-        margin: 0 0 0 0.5rem;
-    }
-
-
+	.calculator-element {
+		margin: 0 0 0 0.5rem;
+	}
 </style>
